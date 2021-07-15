07581f7e80a4c871bf6e227872af56f817ace7af
Gareth Williams <gaz492@gmail.com>
2021-07-15 01:11:24 +0100

    Error time formatting + package update

--- collector.go
+++ collector.go
@@ -216,7 +216,7 @@ func (c *solarCollector) Collect(ch chan<- prometheus.Metric) {
 	c.mutex.Lock() // To protect metrics from concurrent collects.
 	defer c.mutex.Unlock()
 	if err := c.collect(ch); err != nil {
-		pterm.Error.Println(fmt.Sprintf("[%s]Error getting solar controller data: %s", time.Now().UTC(), err))
+		pterm.Error.Println(fmt.Sprintf("[%s]Error getting solar controller data: %s", time.Now().UTC().Format(time.RFC1123Z), err))
 		c.scrapeFailures.Inc()
 		c.scrapeFailures.Collect(ch)
 	}
